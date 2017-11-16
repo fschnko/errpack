f23bb7e9360e3fb81a21300e7bb6824ae2f1538e
fschnko <fschnko@gmail.com>
2017-11-16 22:35:19 +0200

    Repo integration with external services

--- doc.go
+++ doc.go
@@ -1,13 +1,16 @@
-// Package errpack declares the Pack type for errors collecting.
-// This is useful for cyclic or unrelated operations
-// when journaling is not allowed (library packages, etc.).
-//
-// 	errp := New("pack")
-// 	for i := 0; i < 5; i++ {
-// 		errp.Add(someCallWithError(i))
-// 	}
-//
-// 	return errp.ErrorOrNil()
-//
-// NOTE: Currently not being thread safe!
+/*
+Package errpack declares the Pack type for errors collecting.
+This is useful for cyclic or unrelated operations
+when journaling is not allowed (library packages, etc.).
+
+	errp := New("pack")
+	for i := 0; i < 5; i++ {
+		errp.Add(someCallWithError(i))
+	}
+
+	return errp.ErrorOrNil()
+
+
+NOTICE: Currently not being thread safe!
+*/
 package errpack
